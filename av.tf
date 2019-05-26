843f0c4a51ab9b500fe256c9340e48acae711e67
Reid Burke <me@reidburke.com>
2019-05-26 10:34:18 -0700

    Update for 2019-04-28 awakening-nginx-rtmp.

--- av.tf
+++ av.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
     subscription_id = "${var.azure_subscription_id}"
-    client_id = "${var.azure_client_id}"
-    client_secret = "${var.azure_client_secret}"
     tenant_id = "${var.azure_tenant_id}"
 }
 
--- variables.tf
+++ variables.tf
@@ -6,6 +6,4 @@ variable "publish_secret" {}
 variable "azure_region" {}
 variable "azure_tenant_id" {}
 variable "azure_subscription_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
 variable "azure_storage_prefix" {}
